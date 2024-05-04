b43cfae0f9414041c91cdaf8a1e4a23b17871614
Luka Čupić <lukacupic@opennovations.nl>
2024-05-04 15:54:40 +0200

    Update the routing config

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,13 +1,23 @@
 exports.handler = async function (event, context) {
   const url = "https://thevietnamexperience.netlify.app" + event.rawPath;
-  const response = await fetch(url);
-  const data = await response.text();
 
-  return {
-    statusCode: 200,
-    body: data,
-    headers: {
-      "Content-Type": "text/html",
-    },
-  };
+  try {
+    // Use Gridsome's built-in fetch method to make HTTP requests
+    const response = await context.fetch(url);
+    const data = await response.text();
+
+    return {
+      statusCode: 200,
+      body: data,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
+  }
 };
