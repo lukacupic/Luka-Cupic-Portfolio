4c25aa5c162bbfd6c58cfd3ea314a303af0a9c71
Luka Čupić <lukacupic@opennovations.nl>
2024-05-05 22:36:58 +0200

    Mobile navigation almost working

--- src/script.js
+++ src/script.js
@@ -4,12 +4,23 @@ function toggleMenu(e) {
   if (e.name === "menu") {
     e.name = "close";
 
-    navLinks.classList.remove("top-[-100%]");
-    navLinks.classList.add("top-[10%]");
+    navLinks.classList.remove("max-h-0");
+    navLinks.classList.add("max-h-10");
   } else {
     e.name = "menu";
-
-    navLinks.classList.remove("top-[10%]");
-    navLinks.classList.add("top-[-100%]");
+    navLinks.classList.remove("max-h-10");
+    navLinks.classList.add("max-h-0");
   }
 }
+
+function fitMenuToNavBar() {
+  let navElement = document.querySelector("nav");
+  let navLinks = document.getElementById("nav-links");
+
+  let navElementHeight = navElement.offsetHeight;
+  navLinks.style.top = navElementHeight + "px";
+}
+
+window.onload = function () {
+  fitMenuToNavBar();
+};
