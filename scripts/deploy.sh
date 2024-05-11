2c4285574706072297cdf6e7829b8f49109ce142
Luka Čupić <lukacupic@opennovations.nl>
2024-05-11 15:01:51 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@
 TEXT_COLOR='\033[0;34m'
 NO_COLOR='\033[0m'
 
-printf "${TEXT_COLOR}Updating lukacupic.com${NO_COLOR}\n"
+printf "${TEXT_COLOR}Deploying lukacupic.com${NO_COLOR}\n"
 
 printf "${TEXT_COLOR}Pulling the latest changes: ${NO_COLOR}"
 git pull > /dev/null 2>&1
@@ -51,4 +51,4 @@ else
   exit 1
 fi
 
-printf "${TEXT_COLOR}Update successful.${NO_COLOR}\n"
+printf "${TEXT_COLOR}Deploy successful.${NO_COLOR}\n"
