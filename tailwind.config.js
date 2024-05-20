a98ec11021495f672ccdf1a8daf70102817366e1
Luka Čupić <lukacupic@opennovations.nl>
2024-05-20 19:41:12 +0200

    Update interests

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@ module.exports = {
   theme: {
     fontFamily: {
       futura: ["Futura", "sans-serif"],
+      karate: ["karate", "sans-serif"],
     },
     extend: {
       colors: {
         blue: "#0078ff",
         custom_gray: "#5c5c5c",
+      },
+      scale: {
+        130: "1.30",
       },
     },
   },
